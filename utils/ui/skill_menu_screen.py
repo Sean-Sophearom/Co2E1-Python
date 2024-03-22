346ecb6149ac6363ae71ead003cb36eef947b6ab
Sophearom Sean <sophearom.sean@canadiabank.com.kh>
2024-03-22 08:26:58 +0700

    Add SkillSO class and update OneSkillPanel to use SkillSO objects with description

--- utils/ui/skill_menu_screen.py
+++ utils/ui/skill_menu_screen.py
@@ -1,4 +1,5 @@
 from ..sprite.imp import *
+from dataclasses import dataclass
 
 class SkillMenuScreen(pygame.sprite.Sprite):
     def __init__(self):
@@ -16,14 +17,18 @@ class SkillMenuScreen(pygame.sprite.Sprite):
         title_rect = title_surf.get_rect(center=(SCREEN_WIDTH // 2, 100))
 
         skill_names = (
-            "skill",
-            "attack",
-            "defense",
-            "utility"
+            # "skill",
+            # "attack",
+            # "defense",
+            # "utility"
+            SkillSO("Ability", "Unlock a new ability OR Enhance existing ability", "skill", None),
+            SkillSO("Attack", "Increase base attack stats, cooldown or attack range", "attack", None),
+            SkillSO("Defense", "Increase base defense stats, health or durability", "defense", None),
+            SkillSO("Utility", "Increase utility stats, movement speed, or exp", "utility", None)
         )
 
         self.skills = [
-            OneSkillPanel(name, idx) for idx, name in enumerate(skill_names)
+            OneSkillPanel(item.title, item.icon, item.description, idx, item.onclick) for idx, item in enumerate(skill_names)
         ]
 
         self.surf.blit(overlay_surf, overlay_rect)
@@ -52,7 +57,7 @@ class SkillMenuScreen(pygame.sprite.Sprite):
         self.is_hovering = is_hovering
 
 class OneSkillPanel:
-    def __init__(self, png_name, idx, onclick=None):
+    def __init__(self, title, icon, description, idx, onclick=None):
         self.top = 200
         self.skill_y_offset = 64
         self.onclick = onclick
@@ -62,7 +67,7 @@ class OneSkillPanel:
         self.surf = pygame.Surface((SCREEN_WIDTH // 4, SCREEN_HEIGHT - self.top), pygame.SRCALPHA)
         self.rect = self.surf.get_rect(topleft=(idx * SCREEN_WIDTH//4, self.top))
 
-        self.skill_surf = pygame.image.load(f"asset/images/icon_{png_name}.png").convert_alpha()
+        self.skill_surf = pygame.image.load(f"asset/images/icon_{icon}.png").convert_alpha()
         self.skill_original_surf = self.skill_surf
         self.skill_surf = pygame.transform.rotozoom(self.skill_surf, 0, 1.4)
         self.skill_rect = self.skill_surf.get_rect(center=(SCREEN_WIDTH // 8, self.skill_y_offset))
@@ -73,17 +78,23 @@ class OneSkillPanel:
         self.panel_rect = self.panel_surf.get_rect(center=(SCREEN_WIDTH // 8, self.skill_y_offset))
 
         self.font = pygame.font.Font(FONTPATH, 40)
-        self.text_surf = self.font.render(capitalize(png_name), True, (255, 255, 255))
+        self.text_surf = self.font.render(title, True, (255, 255, 255))
         self.text_rect = self.text_surf.get_rect(center=(SCREEN_WIDTH // 8, self.skill_y_offset * 2.5))
         
-        self.description_font = pygame.font.Font(FONTPATH, 25)
-        self.description_text_surf = self.description_font.render(capitalize("Lorem Ipsum Dolor Amet Sit"), True, (255, 255, 255))
-        self.description_text_rect = self.description_text_surf.get_rect(center=(SCREEN_WIDTH // 8, self.skill_y_offset * 3))
+        description_font = pygame.font.Font(FONTPATH, 25)
+
+        renderTextCenteredAt(
+            description,
+            description_font,
+            SCREEN_WIDTH // 8,
+            self.skill_y_offset * 3,
+            self.surf,
+            240
+        )
 
         self.surf.blit(self.panel_surf, self.panel_rect)
         self.surf.blit(self.skill_surf, self.skill_rect)
         self.surf.blit(self.text_surf, self.text_rect)
-        self.surf.blit(self.description_text_surf, self.description_text_rect)
 
     def update(self):
         pass
@@ -112,5 +123,47 @@ class OneSkillPanel:
 
         return left <= x <= right and top <= y <= bottom
 
-def capitalize(string: str):
-    return string[0].upper() + string[1:]
+
+@dataclass
+class SkillSO:
+    title: str
+    description: str
+    icon: str
+    onclick: callable
+
+def renderTextCenteredAt(text, font, x, y, screen, allowed_width,colour=(255,255,255)):
+    # first, split the text into words
+    words = text.split()
+
+    # now, construct lines out of these words
+    lines = []
+    while len(words) > 0:
+        # get as many words as will fit within allowed_width
+        line_words = []
+        while len(words) > 0:
+            line_words.append(words.pop(0))
+            fw, fh = font.size(' '.join(line_words + words[:1]))
+            if fw > allowed_width:
+                break
+
+        # add a line consisting of those words
+        line = ' '.join(line_words)
+        lines.append(line)
+
+    # now we've split our text into lines that fit into the width, actually
+    # render them
+
+    # we'll render each line below the last, so we need to keep track of
+    # the culmative height of the lines we've rendered so far
+    y_offset = 0
+    for line in lines:
+        fw, fh = font.size(line)
+
+        # (tx, ty) is the top-left of the font surface
+        tx = x - fw / 2
+        ty = y + y_offset
+
+        font_surface = font.render(line, True, colour)
+        screen.blit(font_surface, (tx, ty))
+
+        y_offset += fh
