850a744a14210929e547c51b7261403be79a2b8b
Sean Sophearom <sean.sophearom77@gmail.com>
2024-03-24 09:04:39 +0700

    Add different explosion sprite

--- utils/sprite/explosion.py
+++ utils/sprite/explosion.py
@@ -1,11 +1,13 @@
 from .imp import * 
 from .animated import Animated
+max_sprite_img = 7
 
 class Explosion(Animated):
     def __init__(self, center=None):
         if not center: center = (random.randint(20, SCREEN_WIDTH - 20), random.randint(20, SCREEN_HEIGHT - 20))
+        idx = random.randint(0, max_sprite_img)
         super().__init__(
-            "asset/images/explosion.png", 
+            f"asset/images/explosion/{idx % max_sprite_img + 1}.png", 
             width = 550, 
             height = 550, 
             frames = 10, 
